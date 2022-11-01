6b82d10a8a17b7f24bfa1811487181bad6cbfa57
Bregsi Atingsari Julastri <bregsiaju@gmail.com>
2022-11-01 11:56:02 +0700

    modify props class to className

--- src/components/UI/Hero.js
+++ src/components/UI/Hero.js
@@ -7,21 +7,21 @@ import car from '../../assets/general/img_car.png';
 
 const Hero = () => {
   return (
-    <div id="main" class="d-flex justify-content-center">
-      <div class="position-relative">
+    <div id="main" className="d-flex justify-content-center">
+      <div className="position-relative">
         <Container>
           <Row>
             <Col lg={6} className="welcome d-flex align-items-center">
-              <div class="welcome-text">
-                <p class="slogan fw-bold">Sewa & Rental Mobil Terbaik di kawasan Surabaya</p>
-                <p class="fw-light">Selamat datang di Binar Car Rental. Kami menyediakan mobil kualitas dengan harga
+              <div className="welcome-text">
+                <p className="slogan fw-bold">Sewa & Rental Mobil Terbaik di kawasan Surabaya</p>
+                <p className="fw-light">Selamat datang di Binar Car Rental. Kami menyediakan mobil kualitas dengan harga
                   terjangkau. Selalu siap melayani kebutuhanmu untuk sewa mobil selama 24 jam.</p>
                 <Nav.Link href=""><MyButton>Mulai Sewa Mobil</MyButton></Nav.Link>
               </div>
             </Col>
             <Col lg={6}>
-              <div class="img-car mt-lg-0 mt-4">
-                <img src={car} class="position-absolute" alt="car" />
+              <div className="img-car mt-lg-0 mt-4">
+                <img src={car} className="position-absolute" alt="car" />
               </div>
             </Col>
           </Row>
