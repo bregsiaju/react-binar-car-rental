5a75d7f3a66229d8b850e9ff012fa387cd886bcd
Bregsi Atingsari Julastri <bregsiaju@gmail.com>
2022-11-02 11:53:56 +0700

    add card component WhyUs section

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import OurService from './components/LandingPage/OurService';
+import WhyUs from './components/LandingPage/WhyUs';
 import Footer from './components/UI/Footer';
 import Hero from './components/UI/Hero';
 import Navbar from './components/UI/NavigationBar';
+import './App.css';
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <div id="service"></div>
       <div className="body-container">
         <OurService />
+        <WhyUs />
       </div>
       <Footer />
     </>
